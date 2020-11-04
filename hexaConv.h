e2151fcb4a8d9e74e2f7a064081baa42bf62f4da
Mistguard <alantournabien@hotmail.fr>
2020-11-04 09:52:39 +0000

    Création fonction de lecture instruction

--- hexaConv.h
+++ hexaConv.h
@@ -2,4 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
+<<<<<<< Updated upstream
 void lireEnreDonnees(char nomFichier1[],char nomFichier2[]);
+=======
+
+>>>>>>> Stashed changes
---
+++ readIn.c
@@ -0,0 +1,11 @@
+#include "readIn.h"
+
+void inJtoHex(char in[], char hex[9])
+{
+	if(char[0]=='J'){
+		for(int i = 0; i < 6; ++i)
+		{
+			
+		}
+	}
+}
