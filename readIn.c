3e029918808f0598532fc5397676232cb78874eb
Gunhas <73703375+Gunhas@users.noreply.github.com>
2020-11-06 11:57:07 +0100

    base de données entière

--- readIn.c
+++ readIn.c
@@ -1,7 +1,52 @@
 #include "readIn.h"
 #include "hexaConv.h"
 
-void inJtoHex(char inst[], int hexa)
+void inAtoHex(char inst[], int hexa)
+{
+	int l = strlen(inst);
+
+	if(strstr(inst,"ADD ")){
+		hex = ADD;
+		hex += atoi(strchr(inst,' ')+1);
+	}else if(strstr(inst, "ADDI")){
+		hex = ADDI << 26;
+		hex += atoi(strchr(inst,' ')+1);
+	}else if(strstr(inst, "AND")){
+		hex = AND;
+		hex += atoi(strchr(inst,' ')+1);
+	}
+}
+
+void inBtoHex(char inst[], int hexa)
+{
+	int l = strlen(inst);
+
+	if(strstr(inst,"BEQ")){
+		hex = BEQ << 26;
+		hex += atoi(strchr(inst,' ')+1);
+	}else if(strstr(inst, "BGTZ")){
+		hex = BGTZ << 26;
+		hex += atoi(strchr(inst,' ')+1);
+	}else if(strstr(inst, "BLEZ")){
+		hex = BLEZ << 26;
+		hex += atoi(strchr(inst,' ')+1);
+	}else if(strstr(inst, "BNE")){
+		hex = BNE << 26;
+		hex += atoi(strchr(inst,' ')+1);
+	}
+}
+
+void inDtoHex(char inst[], int hexa)
+{
+	int l = strlen(inst);
+
+	if(strstr(inst,"DIV")){
+		hex = DIV;
+		hex += atoi(strchr(inst,' ')+1);
+	}
+}
+
+void inJtoHex(char inst[], int hexa);
 {
 	int l = strlen(inst);
 
@@ -11,15 +56,105 @@ void inJtoHex(char inst[], int hexa)
 	}else if(strstr(inst, "J ")){
 		hex = J << 26;
 		hex += atoi(strchr(inst,' ')+1);
+	}else if(strstr(inst, "JR")){
+		hex = JR;
+		hex += atoi(strchr(inst,' ')+1);
 	}
 }
 
-void inItoHex(char inst[], int hexa)
+void inLtoHex(char inst[], int hexa)
 {
-	
+	int l = strlen(inst);
+
+	if(strstr(inst,"LUI")){
+		hex = LUI << 26;
+		hex += atoi(strchr(inst,' ')+1);
+	}else if(strstr(inst, "LW")){
+		hex = LW << 26;
+		hex += atoi(strchr(inst,' ')+1);
+	}
+}
+
+void inMtoHex(char inst[], int hexa)
+{
+	int l = strlen(inst);
+
+	if(strstr(inst,"MFHI")){
+		hex = MFHI;
+		hex += atoi(strchr(inst,' ')+1);
+	}else if(strstr(inst, "MFLO")){
+		hex = MFLO;
+		hex += atoi(strchr(inst,' ')+1);
+	}else if(strstr(inst, "MULT")){
+		hex = MULT;
+		hex += atoi(strchr(inst,' ')+1);
+	}
+}
+
+void inNtoHex(char inst[], int hexa)
+{
+	int l = strlen(inst);
+
+	if(strstr(inst,"NOP")){
+		hex = NOP;
+		hex += atoi(strchr(inst,' ')+1);
+	}
+}
+
+void inOtoHex(char inst[], int hexa)
+{
+	int l = strlen(inst);
+
+	if(strstr(inst,"OR")){
+		hex = OR;
+		hex += atoi(strchr(inst,' ')+1);
+	}
 }
 
 void inRtoHex(char inst[], int hexa)
 {
+	int l = strlen(inst);
 
+	if(strstr(inst,"ROTR")){
+		hex = ROTR;
+		hex += atoi(strchr(inst,' ')+1);
+	}
 }
+
+void inStoHex(char inst[], int hexa)
+{
+	int l = strlen(inst);
+
+	if(strstr(inst,"SLL")){
+		hex = SLL;
+		hex += atoi(strchr(inst,' ')+1);
+	}else if(strstr(inst, "SLT")){
+		hex = SLT;
+		hex += atoi(strchr(inst,' ')+1);
+	}else if(strstr(inst, "SRL")){
+		hex = SRL;
+		hex += atoi(strchr(inst,' ')+1);
+	}else if(strstr(inst, "SUB")){
+		hex = SUB;
+		hex += atoi(strchr(inst,' ')+1);
+	}else if(strstr(inst, "SW")){
+		hex = SW << 26;
+		hex += atoi(strchr(inst,' ')+1);
+	}else if(strstr(inst, "SYSCALL")){
+		hex = SYSCALL;
+		hex += atoi(strchr(inst,' ')+1);
+	}
+}
+
+void inXtoHex(char inst[], int hexa)
+{
+	int l = strlen(inst);
+
+	if(strstr(inst,"XOR")){
+		hex = XOR;
+		hex += atoi(strchr(inst,' ')+1);
+	}
+}
+
+
+
