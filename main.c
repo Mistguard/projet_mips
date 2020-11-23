90e8c9aaa82e85fe2ca0f53acbde742e493f79e8
Mistguard <alantournabien@hotmail.fr>
2020-11-23 15:29:58 +0000

    shits

--- main.c
+++ main.c
@@ -2,7 +2,11 @@
 
 int main(int argc, char *argv[])
 {
+	
+
 	/* ici on récupère le nom du fichier a lire lors de l'execution de la commande dans l'invité de commande grace a argv[]*/
 	lireEnreDonnees(argv[1],argv[2]);
+
+	
 	return 0;
 }
