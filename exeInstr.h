dfc1b6558779684940b2a20f928f54bcb34a8dce
Mistguard <alantournabien@hotmail.fr>
2020-12-07 16:35:26 +0000

    start execution of instruction

---
+++ exeInstr.h
@@ -0,0 +1,19 @@
+#ifndef EXEINSTR_H
+#define EXEINSTR_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    int opcode;
+    int rd;
+    int rs;
+    int rt;
+    int im; // sa == target == offset
+}Instrct;
+
+void execInstr(Instrct* instr, int[] regs, int[] mem);
+
+#endif
